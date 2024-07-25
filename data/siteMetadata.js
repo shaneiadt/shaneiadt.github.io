e5ceb6394b9458d105c8ec388fd140bdd1e70a23
shaneiadt <shaneiadt@gmail.com>
2024-07-25 15:31:04 +0100

    feat: migrate css houdini post

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -17,7 +17,7 @@ const siteMetadata = {
   // twitter: 'https://twitter.com/Twitter',
   // facebook: 'https://facebook.com',
   // youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com/in/shane-o-moore/',
+  linkedin: 'https://www.linkedin.com/in/shane-o-moore',
   // threads: 'https://www.threads.net',
   // instagram: 'https://www.instagram.com',
   locale: 'en-IE',
